2791614570a3c7099bd5fe8cf84c391b9c62bcf8
kyle67 <daniels.kyle@gmail.com>
2020-10-21 22:09:37 -0400

    created handlebars template for store.html

--- public/main.js
+++ public/main.js
@@ -33,14 +33,10 @@ const context = {
     ]
   };
 
-  const templateElement = document.getElementById('templateHB');
-
+  const templateElement = document.getElementById("templateHB");
   const templateSource = templateElement.innerHTML
-
   const template = Handlebars.compile(templateSource);
-
   const compiledHtml = template(context);
-
   document.getElementById('information').innerHTML = compiledHtml;
 
 
